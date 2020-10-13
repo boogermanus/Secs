7cda8d3a33d16ba481405362cb44816e9abb769c
jordan.woodruff <jordan.woodruff@tylertech.com>
2020-10-12 19:11:02 -0500

    add interface

---
+++ src/app/interfaces/icalculation-options.ts
@@ -0,0 +1,11 @@
+export interface ICalculationOptions {
+    mode: string;
+    years?: number;
+    days?: number;
+    hours?: number;
+    minutes?: number;
+    seconds?: number;
+    gigaSeconds?: number;
+    megaSeconds?: number;
+    kiloSecods?: number;
+}
