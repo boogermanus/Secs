ac3a67165345959a5b24cf0f099ec61d83224944
jordan.woodruff <jordan.woodruff@tylertech.com>
2020-10-20 19:38:21 -0500

    massive refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
-import { NormalTimeComponent } from './normal-time/normal-time.component';
-import { ExpandedTimeComponent } from './expanded-time/expanded-time.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NormalTimeComponent,
-    ExpandedTimeComponent
   ],
   imports: [
     BrowserModule,
--- src/app/models/mode-constants.ts
+++
@@ -1,9 +0,0 @@
-export class ModeConstants {
-    public static readonly NORMAL = 'n';
-    public static readonly EXPANDED = 'e';
-    private static readonly _modes: string[] = [ModeConstants.NORMAL, ModeConstants.EXPANDED];
-
-    public static modes(): string[] {
-        return this._modes;
-    }
-}
