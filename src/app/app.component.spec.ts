038cf6759c34a9d577eb9f34b21f3e14271f007a
jordan.woodruff <jordan.woodruff@tylertech.com>
2022-12-02 19:45:01 -0600

    compiling

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,67 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
+import * as moment from 'moment';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [
+        NgbModule,
+      ]
     }).compileComponents();
+
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
-  it(`should have as title 'Secs'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('Secs');
+  it('should have method calculate', () => {
+    expect(component.calculate).toBeDefined();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Secs app is running!');
+  it('should have method updateStartTime', () => {
+    expect(component.updateStartTime).toBeDefined();
+  });
+
+  it('should have method updateEndTime', () => {
+    expect(component.updateEndTime).toBeDefined();
+  });
+
+  describe('calculate()', () => {
+    it('should set calculation to be defined', () => {
+      component.calculate();
+      expect(component.calculation).toBeDefined();
+    });
+  });
+
+  describe('updateStartTime()', () => {
+    it('should update start', () => {
+      const now = moment();
+      component.updateStartTime(now);
+
+      expect(component.start).toEqual(now);
+    });
+  });
+
+  describe('updateEndTime()', () => {
+    it('should update end', () => {
+      const now = moment();
+      component.updateEndTime(now);
+
+      expect(component.end).toEqual(now);
+    });
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { SecsDateTimeComponent } from './secs-date-time/secs-date-time.component';
+import { SecsDisplayComponent } from './secs-display/secs-display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SecsDateTimeComponent,
+    SecsDisplayComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
     NgbModule,
-    BrowserAnimationsModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
